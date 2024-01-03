9917104c8b4f88b584fe0be994fba6d0f276816c
Sang Hoon Kim <KSHoon522@gamil.com>
2024-01-03 16:29:15 +0900

    Patterns / Data fetching

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,16 @@
-import NavBar from "@/components/NavBar";
-import "../styles/globals.css";
+import Layout from "@/components/Layout";
 
 export default function CustomApp({ Component, pageProps }) {
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
       <style jsx global>{`
-        a {
-          color: white;
+        body {
+          max-width: 520px;
+          width: 100%;
+          margin: 0 auto;
         }
       `}</style>
-    </>
+    </Layout>
   );
 }
